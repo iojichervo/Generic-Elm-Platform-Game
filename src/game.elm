81b0c92a12632affd7ccd1d7e8539bd118a02250
ioji <ioji@chervonagura.com.ar>
2016-02-06 13:44:29 -0300

    Initial files

---
+++ src/game.elm
@@ -0,0 +1,240 @@
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
+import Keyboard
+import Time exposing (..)
+import Window
+import Text
+import Debug
+
+-- CONSTANTS
+
+leftWall : Platform
+leftWall =
+  Platform 1300 10 -300 0
+
+rightWall : Platform
+rightWall =
+  Platform 1300 10 300 0
+
+-- MODEL
+
+type State = Playing | Over
+
+type alias Game =
+  { state : State
+  , mario : Model
+  , platforms : List Platform
+  }
+
+
+type alias Model =
+  { x : Float
+  , y : Float
+  , vx : Float
+  , vy : Float
+  , dir : Direction
+  }
+
+type alias Platform =
+  { h : Int
+  , w : Int
+  , x : Float
+  , y : Float
+  }
+
+type Direction = Left | Right
+
+
+type alias Keys = { x:Int, y:Int }
+
+
+initialMario : Model
+initialMario =
+  { x = 0
+  , y = 0
+  , vx = 0
+  , vy = 0
+  , dir = Right
+  }
+
+
+defaultGame : Game
+defaultGame =
+  { state = Playing
+  , mario = initialMario
+  , platforms = []
+  }
+
+
+emptyPlatform : Platform
+emptyPlatform =
+  { h = 0
+  , w = 0
+  , x = 0
+  , y = 0
+  }
+
+-- UPDATE
+
+update : (Float, Keys) -> Game -> Game
+update (dt, keys) game =
+  { game |
+    mario = updateMario (dt, ((Debug.log "keys") keys)) ((Debug.log "mario") game.mario),
+    platforms = platformGenerator dt :: game.platforms
+  }
+
+
+updateMario : (Float, Keys) -> Model -> Model
+updateMario (dt, keys) mario =
+  mario
+    |> gravity dt
+    |> jump keys
+    |> walk keys
+    |> physics dt
+
+
+jump : Keys -> Model -> Model
+jump keys mario =
+  if keys.y > 0 && mario.vy == 0 then
+      { mario | vy = 8.0 }
+  else
+      mario
+
+
+gravity : Float -> Model -> Model
+gravity dt mario =
+  { mario |
+      vy = if mario.y > 0 then mario.vy - dt/4 else 0
+  }
+
+
+physics : Float -> Model -> Model
+physics dt mario =
+  { mario |
+      x = mario.x + dt * mario.vx,
+      y = max 0 (mario.y + dt * mario.vy)
+  }
+
+
+walk : Keys -> Model -> Model
+walk keys mario =
+  { mario |
+      vx = (toFloat keys.x * 2),
+      dir =
+        if keys.x < 0 then
+            Left
+
+        else if keys.x > 0 then
+            Right
+
+        else
+            mario.dir
+  }
+
+platformGenerator : Float -> Platform
+platformGenerator dt =
+  Platform 20 100 (10*dt) (10*dt)
+  {-let
+    platform =
+      { h = 20 
+      , w = 100
+      , x = 10 * dt
+      , y = 10 * dt
+      }
+  in
+    platform-}
+
+within : Model -> Platform -> Bool
+within mario platform = 
+  near platform.x ((toFloat platform.h) / 2) mario.x && near platform.y ((toFloat platform.w) / 2) mario.y
+
+near : number -> number -> number -> Bool
+near c h n =
+  n >= c-h && n <= c+h
+
+stepV : number -> Bool -> Bool -> number
+stepV v lowerCollision upperCollision =
+  if lowerCollision then
+      abs v
+  else if upperCollision then
+      -(abs v)
+  else
+      v
+
+-- VIEW
+
+view : (Int, Int) -> Game -> Element
+view (w',h') game =
+  let
+    (w,h) = (toFloat w', toFloat h')
+
+    verb =
+      if game.mario.y > 0 then
+          "jump"
+
+      else if game.mario.vx /= 0 then
+          "walk"
+
+      else
+          "stand"
+
+    dir =
+      case game.mario.dir of
+        Left -> "left"
+        Right -> "right"
+
+    src =
+      "../img/mario/"++ verb ++ "/" ++ dir ++ ".gif"
+
+    marioImage =
+      image 35 35 src
+
+    groundY = 62 - h/2
+
+    position =
+      (game.mario.x, game.mario.y + groundY)
+
+    platform = if List.isEmpty game.platforms then emptyPlatform else fromJust (List.head ((Debug.log "platforms") game.platforms))
+    platRect = filled Color.blue (rect (toFloat platform.w) (toFloat platform.h))
+
+  in
+    collage w' h'
+      [ rect w h
+          |> filled Color.white
+      , rect w 50
+          |> filled Color.charcoal
+          |> move (0, 24 - h/2)
+      , rect (toFloat leftWall.w) (toFloat leftWall.h) -- left wall
+          |> filled Color.red
+          |> move (leftWall.x, leftWall.y)
+      , rect (toFloat rightWall.w) (toFloat rightWall.h) -- right wall
+          |> filled Color.red
+          |> move (rightWall.x, rightWall.y)
+      , marioImage
+          |> toForm
+          |> move position
+      , toForm (leftAligned (Text.fromString (toString game.mario.x)))
+          |> move (0, 40 - h/2)
+      ,  platRect
+          |> move (platform.x, platform.y)
+      ]
+
+fromJust : Maybe a -> a
+fromJust x = case x of
+    Just y -> y
+    Nothing -> Debug.crash "error: fromJust Nothing"
+
+-- SIGNALS
+
+main : Signal Element
+main =
+  Signal.map2 view Window.dimensions (Signal.foldp update defaultGame input)
+
+
+input : Signal (Float, Keys)
+input =
+  let
+    delta = Signal.map (\t -> t/20) (fps 35)
+  in
+    Signal.sampleOn delta (Signal.map2 (,) delta Keyboard.arrows)
